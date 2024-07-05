d97604adbfd95e4616c85f401c8e8d246130d390
5n34kyp3t3 <malhotra.arnav.201@gmail.com>
2024-07-05 23:23:03 +0530

    finished 16 - Three Sum Closest

--- 16-3sum_closest.hpp
+++ 16-3sum_closest.hpp
@@ -30,6 +30,7 @@ Constraints:
 
 #include <stdio.h>
 #include <vector>
+#include <algorithm>
 #ifndef Solution16_H
 #define Solution16_H
 
@@ -41,8 +42,56 @@ public:
     int threeSumClosest(vector<int> &nums, int target)
     {
         // right away incorporate for the case when the nums.length is three and the answer would be sum of its three elements
+        int len = nums.size();
 
+        if (len==3) return nums[0] + nums[1] + nums[2];
 
+        sort(nums.begin(),nums.end());
+
+        int smallest = nums[0] + nums[1] + nums[2];
+        if (smallest>=target) return smallest; //because any number after nums[2] is >= the first three thereby not changing or decreasing the sum.
+
+        int max = nums[len-1] + nums[len-2] + nums[len-3];
+        if (max <= target ) return max; // similarly any number before nums[len-3] is <= the last three therby not changing or increasing the sum.
+
+        int perm =  nums[0];
+        int left, right, sum, num;
+        int dist = abs(smallest - target);
+        
+        for (int i = 0; i<len-2;i++){
+            if ( i && nums[i] == perm){
+                continue;
+            }
+            //update perm when you move forward in the for loop and increase the left and right pointers.
+            perm = num = nums[i];
+            left = i + 1 ;
+            right = len -1;
+            while ( left < right) {
+                sum = num + nums[left] + nums[right];
+                if (sum == target) return sum;
+                // if difference between sum and target is lesser than the previous difference between smallest and target, assign perm to the sum variable and also update the dist to the one between the new sum and target
+                if (abs(sum - target)< dist){ 
+                    smallest = sum;
+                    dist = abs(smallest - target);
+
+                }
+                //if sum < target and next number for the left pointer is the same, keep moving forward. (so that we skip redundant cycles)
+                if (sum < target){
+                    while (left < right && nums[left] ==nums[left+1]){
+                        left ++;
+                    }
+                    left++;
+                }
+                //if sum > target and previous number for the right pointer is the same, keep moving backward. (so that we skip redundant cycles)
+                else{
+                    while (left < right && nums[right] ==nums[right-1]){
+                        right--;
+                }
+                right--;
+            }
+            
+        }
+        return smallest; 
     }
 };
 
